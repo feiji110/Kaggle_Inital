e92d6eea2406bdff2a13e0dc4306557536fdf4c8
Angus Gao <feiji110@protonmail.com>
2020-08-09 14:20:49 +0800

    Create 200809_标准库中的数学和统计.py
    math.radians() degrees() hypot() sqrt() factorial() log() pow()函数

---
+++ 200809_标准库中的数学和统计.py
@@ -0,0 +1,42 @@
+# import math
+# math.pi
+# math.e
+
+# math.fabs(98.6)
+# math.fabs(-271.1)#绝对值
+
+# math.floor(98.6)#98
+# math.ceil(98.6)#99
+# math.floor(-271.1)#-272
+# math.ceil(-271.1)#-271
+
+# math.factorial(0)#n!
+# math.factorial(10)
+
+# math.log(1.0)#0.0
+# math.log(math.e)#1.0
+
+# math.log(8,2)# 3.0
+# math.pow(2,3)# 8.0
+# 2**3 # 8
+# 2.0**3 # 8.0
+
+# math.sqrt(100.0)# 10.0
+# math.hypot(x,y) 勾股定理求斜边
+# math.hypot(3,4)# 5.0
+
+# math.sin(2*math.pi)
+# math.cos()
+# math.tan()
+# math.asin()
+# math.acos()
+# math.atan()
+# math.atan2()
+# x = 3.0
+# y = 4.0
+# math.hypot(x,y)
+# math.sqrt(x*x+y*y)
+# math.sqrt(x**2+y**2)
+# 角坐标转换
+# math.radians(180.0)# 3.1415... radian 弧度
+# math.degrees(math.pi)# 180.0
